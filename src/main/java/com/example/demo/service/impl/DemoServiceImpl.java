eb07ff1a1bd0e7319d1fcc8ffaa4456dec079478
Aaron-zheng <fucheng.zheng@gmail.com>
2017-06-22 18:14:44 +0800

    更新分步方法

--- src/main/java/com/example/demo/service/impl/DemoServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DemoServiceImpl.java
@@ -7,8 +7,7 @@ import com.example.demo.fabric.sdkintegration.SampleStore;
 import com.example.demo.fabric.sdkintegration.SampleUser;
 import com.example.demo.service.DemoService;
 
-import org.hyperledger.fabric.sdk.Enrollment;
-import org.hyperledger.fabric.sdk.HFClient;
+import org.hyperledger.fabric.sdk.*;
 import org.hyperledger.fabric.sdk.security.CryptoSuite;
 import org.hyperledger.fabric_ca.sdk.HFCAClient;
 import org.hyperledger.fabric_ca.sdk.RegistrationRequest;
@@ -17,7 +16,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.nio.file.Paths;
 import java.util.Collection;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.String.format;
 
 @Service
 public class DemoServiceImpl implements DemoService {
@@ -36,7 +40,7 @@ public class DemoServiceImpl implements DemoService {
 
     public static void main(String[] args) throws Exception{
 
-        //////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////初始化，保存文件
         HFClient client = HFClient.createNewInstance();
         client.setCryptoSuite(CryptoSuite.Factory.getCryptoSuite());
         LOGGER.info("java.io.tmpdir: " + System.getProperty("java.io.tmpdir"));
@@ -46,6 +50,7 @@ public class DemoServiceImpl implements DemoService {
 
 
         //////////////////////////////////////////////////////获取组织
+        //sampleOrg的名称为peerOrg1
         testSampleOrgs = testConfig.getIntegrationTestsSampleOrgs();
         sampleOrg = testSampleOrgs.iterator().next();
         sampleOrg.setCAClient(HFCAClient.createNewInstance(sampleOrg.getCALocation(), sampleOrg.getCAProperties()));
@@ -56,25 +61,91 @@ public class DemoServiceImpl implements DemoService {
         ca.setCryptoSuite(CryptoSuite.Factory.getCryptoSuite());
         SampleUser admin = sampleStore.getMember("admin", sampleOrg.getName());
         //enrollment这步会在，本地生成keypair和csr，然后发送到fabric-ca进行签名（enrollment会包含，私钥和数字证书）
-        Enrollment enrollment = ca.enroll(admin.getName(), "adminpw");
-        admin.setEnrollment(enrollment);
+        Enrollment adminEnrollment = ca.enroll(admin.getName(), "adminpw");
+        admin.setEnrollment(adminEnrollment);
         admin.setMPSID(sampleOrg.getMSPID());
         //该机构的admin
         sampleOrg.setAdmin(admin);
 
 
-        //////////////////////////////////////////////////////会员注册（不可以重复注册）
-        SampleUser user = sampleStore.getMember("user3", sampleOrg.getName());
+        //////////////////////////////////////////////////////会员注册（同一个用户名，不可以重复注册）
+        SampleUser user = sampleStore.getMember("user1", sampleOrg.getName());
         //affiliation
         RegistrationRequest registrationRequest = new RegistrationRequest(user.getName(), "org1.department1");
         //发送用户名+部门名，到fabric-ca进行注册，返回值只有一个密码
         String enrollmentSecret = ca.register(registrationRequest, admin);
         user.setEnrollmentSecret(enrollmentSecret);
+        //获取enrollment
+        Enrollment userEnrollment = ca.enroll(user.getName(), user.getEnrollmentSecret());
+        user.setEnrollment(userEnrollment);
+        user.setMPSID(sampleOrg.getMSPID());
         sampleOrg.addUser(user);
+
+
+        //////////////////////////////////////////////////////设置peerOrgAdmin
+        String sampleOrgName = sampleOrg.getName();
+        String sampleOrgDomainName =sampleOrg.getDomainName();
+        //参数为name, org, MSPID, privateKeyFile,certificateFile
+        SampleUser peerOrgAdmin = sampleStore.getMember(sampleOrgName + "Admin", sampleOrgName, sampleOrg.getMSPID(),
+                findFile_sk(Paths.get(testConfig.getTestChannlePath(), "crypto-config/peerOrganizations/",
+                        sampleOrgDomainName, format("/users/Admin@%s/msp/keystore", sampleOrgDomainName)).toFile()),
+                Paths.get(testConfig.getTestChannlePath(), "crypto-config/peerOrganizations/", sampleOrgDomainName,
+                        format("/users/Admin@%s/msp/signcerts/Admin@%s-cert.pem", sampleOrgDomainName, sampleOrgDomainName)).toFile());
+        //该peerOrgAdmin是用来创建channel，添加peer，和安装chaincode的
+        sampleOrg.setPeerAdmin(peerOrgAdmin);
+
+
+        //////////////////////////////////////////////////////创建orderer
+        String orderName = sampleOrg.getOrdererNames().iterator().next();
+        Properties ordererProperties = testConfig.getOrdererProperties(orderName);
+        //设置keepAlive
+        ordererProperties.put("grpc.NettyChannelBuilderOption.keepAliveTime", new Object[] {5L, TimeUnit.MINUTES});
+        ordererProperties.put("grpc.NettyChannelBuilderOption.keepAliveTimeout", new Object[] {8L, TimeUnit.SECONDS});
+        Orderer orderer = client.newOrderer(orderName, sampleOrg.getOrdererLocation(orderName), ordererProperties);
+
+
+        //////////////////////////////////////////////////////创建channel
+        client.setUserContext(sampleOrg.getPeerAdmin());
+        ChannelConfiguration channelConfiguration = new ChannelConfiguration(new File("src/main/java/com/example/demo/fabric/fixture/sdkintegration/e2e-2Orgs/channel/foo.tx"));
+        //channelName, orderer, channelConfiguration, channelConfigurationSignatures
+        Channel fooChannel = client.newChannel("foo", orderer, channelConfiguration,
+                client.getChannelConfigurationSignature(channelConfiguration, sampleOrg.getPeerAdmin())
+                );
+
+
+        //////////////////////////////////////////////////////创建peer
+        String peerName = sampleOrg.getPeerNames().iterator().next();
+        String peerLocation = sampleOrg.getPeerLocation(peerName);
+        Properties peerProperties = new Properties();
+        //举例说明，设置一些特别的参数
+        peerProperties.put("grpc.NettyChannelBuilderOption.maxInboundMessageSize", 9000000);
+        Peer peer = client.newPeer(peerName, peerLocation, peerProperties);
+        fooChannel.joinPeer(peer);
+        sampleOrg.addPeer(peer);
+
+
+
+
 
 
 
     }
 
+
+    private static File findFile_sk(File directory) {
+
+        File[] matches = directory.listFiles((dir, name) -> name.endsWith("_sk"));
+
+        if (null == matches) {
+            throw new RuntimeException(format("Matches returned null does %s directory exist?", directory.getAbsoluteFile().getName()));
+        }
+
+        if (matches.length != 1) {
+            throw new RuntimeException(format("Expected in %s only 1 sk file but found %d", directory.getAbsoluteFile().getName(), matches.length));
+        }
+
+        return matches[0];
+
+    }
 
 }
